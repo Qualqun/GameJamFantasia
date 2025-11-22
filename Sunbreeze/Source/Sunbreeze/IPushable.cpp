12a8cbafbae097ae4311f69b91a9ea44bcd73586
Qualqun <161536277+Qualqun@users.noreply.github.com>
2025-11-22 02:14:00 +0100

    Move block comportement

--- Sunbreeze/Source/Sunbreeze/IPushable.cpp
+++ Sunbreeze/Source/Sunbreeze/IPushable.cpp
@@ -4,3 +4,8 @@
 #include "IPushable.h"
 
 // Add default functionality here for any IIPushable functions that are not pure virtual.
+
+//FVector IIPushable::PushObject(const FVector& Direction)
+//{
+//	return Direction;
+//}
--- Sunbreeze/Source/Sunbreeze/IPushable.h
+++ Sunbreeze/Source/Sunbreeze/IPushable.h
@@ -7,14 +7,14 @@
 #include "IPushable.generated.h"
 
 // This class does not need to be modified.
-UINTERFACE(MinimalAPI)
+UINTERFACE(BlueprintType)
 class UIPushable : public UInterface
 {
 	GENERATED_BODY()
 };
 
 /**
- * 
+ *
  */
 class SUNBREEZE_API IIPushable
 {
@@ -22,4 +22,7 @@ class SUNBREEZE_API IIPushable
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Push")
+	FVector PushObject(const FVector& Direction);
 };
